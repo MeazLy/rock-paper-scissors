1ff12c6dd719a0e9e08cbcfc585fcc0f560414de
Morgan <mewanchuk@live.com>
2022-07-08 15:30:02 -0600

    updated rock paper scissors UI

--- jsscript.js
+++ jsscript.js
@@ -21,34 +21,59 @@ function computerPlay(){
 }
 let userWinCount = 0;
 let computerWinCount = 0;
-let userName = prompt("What is your name?")
+let tieCount = 0;
+
+const buttons = document.querySelectorAll("button")
 
 
-function playGame(){
-    let playerSelection = prompt("What is your choice?").toLowerCase()
+const onClick = (event) => {
+    let thisChoice = (event.srcElement.id)
     let computerSelection = computerPlay()
-    if(playerSelection === computerSelection){
-        console.log("You tied!")
+    playGame(thisChoice, computerSelection)
+}
+buttons.forEach((button) => {
+    button.addEventListener("click", onClick)
+  
+})
+
+
+function playGame(playerSelection,computerSelection){
+
+    playerSelection = playerSelection.toLowerCase()
+    
+    if(userWinCount >= 5 || computerWinCount >= 5){
+        document.querySelector(".finalResult").innerHTML = "Game over!"
+    }
+    else if(playerSelection === computerSelection){
+        document.querySelector(".tiedCount").innerHTML = "Tied count is " + (tieCount +=1)
+        document.querySelector(".gameResult").innerHTML = `You both picked ${playerSelection}, it's a tie!`
     }else if(
         playerSelection === "rock" && computerSelection === "scissors" || playerSelection === "paper" && computerSelection === "rock" || playerSelection === "scissors" && computerSelection === "paper"
-    ){
-        userWinCount += 1
-        console.log(playerSelection + " beats " + computerSelection + ". You win!")
-    }else if(
-        playerSelection === "rock" && computerSelection === "paper" || playerSelection === "paper" && computerSelection === "scissors" ||
-        playerSelection === "scissors" && computerSelection === "rock"
-    ){
-        computerWinCount += 1
-        console.log(computerSelection + " beats " + playerSelection + ". You lost!")
+        ){
+            document.querySelector(".userWins").innerHTML = "Your score is " + (userWinCount += 1)
+            document.querySelector(".gameResult").innerHTML = playerSelection + " beats " + computerSelection + ". You win!"
+      
+        }else if(
+            playerSelection === "rock" && computerSelection === "paper" || playerSelection === "paper" && computerSelection === "scissors" ||
+            playerSelection === "scissors" && computerSelection === "rock"
+            ){
+                document.querySelector(".computerWins").innerHTML = "Computers score is " + (computerWinCount += 1)
+             
+                document.querySelector(".gameResult").innerHTML = `${computerSelection} beats ${playerSelection}, you lose!`
+                
+            }else{
+                console.log("Invalid selection")
+            }
+        }
 
-}else{
-    console.log("Invalid selection")
-}
-}
-function fiveRounds(){
-    for(let i = 0; i < 5; i++){
-        playGame()
-        console.log(userName + "s " + `score is ${userWinCount}` + " to " + `${computerWinCount}`)
-    }
-}
-fiveRounds()
+        
+        
+        
+
+// function fiveRounds(){
+//     for(let i = 0; i < 5; i++){
+//         playGame()
+//         console.log(userName + "s " + `score is ${userWinCount}` + " to " + `${computerWinCount}`)
+//     }
+// }
+// fiveRounds()
